f50e0ee3e0da1c8dda6ff9b1a1fca09d47d0bcb2
tmabhishek <abhishek100898@gmail.com>
2024-12-20 19:31:54 +0530

    First commit

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(){
+    println("Please enter your age:")
+    val age = readLine()?.toInt()
+    if(age != null){
+        if(age in 0..17){
+            println("You are not yet adult")
+        } else if (age in 18..65){
+            println("your are an adult")
+        }else{
+            println("your are tooooooo old :(")
+        }
+    }
+}
